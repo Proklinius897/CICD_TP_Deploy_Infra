777ae4fb0bf5e9dd195c8314eaf2fac52f84b3c8
Antoine Ansari <49979277+Mohsen51@users.noreply.github.com>
2021-07-15 10:38:09 +0200

    Commit

--- CICD_Infra.tf
+++ CICD_Infra.tf
@@ -79,7 +79,7 @@ resource "aws_route_table" "public" {
   }
 }
 
-# Public Route Table Association
+# Public Route Table Association.
 resource "aws_route_table_association" "public-1" {
   subnet_id      = aws_subnet.subnet-public-1.id
   route_table_id = aws_route_table.public.id
